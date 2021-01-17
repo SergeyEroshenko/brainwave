119a14ee8400ca4302fb6c34a0aea886d238470d
Sergey Eroshenko <sergey.eroshenko.nsk@gmail.com>
2021-01-17 16:05:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+sudo apt-get libav
+python -m venv .env
+pip install --upgrade pip
+pip install pydub
---
+++ run.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+from glob import glob
+
+import pydub
+from pydub import AudioSegment
+from pydub.generators import Sine
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('path', help='Path to music file.')
+parser.add_argument('freq', help='Frequency of binaural wave.')
+
+args = parser.parse_args()
+
+path = args.path
+bias = args.freq.replace(',', '.')
+
+if not os.path.exists(path):
+    raise OSError('File not exists.')
+
+try:
+    bias = float(bias)
+except Exception as ex:
+    print(ex)
+
+file_name = os.path.basename(path)
+name, ext = os.path.splitext(file_name)
+save_name = f'{name}_{bias}{ext}'
+# read audio file
+track = AudioSegment.from_mp3(path)
+# generate two sine waves tracks
+base_tone = Sine(freq=200).to_audio_segment(duration=len(track))
+up_tone = Sine(freq=200 + bias).to_audio_segment(duration=len(track))
+# merge two sine waves to stereo track
+add_sounds = AudioSegment.from_mono_audiosegments(base_tone, up_tone)
+# make a sine waves twice as quiet as a origin file 
+add_sounds = add_sounds + track.dBFS - 6
+# mixing all
+combined = add_sounds.overlay(track)
+# save mix to file
+combined.export(os.path.join('processed', save_name), format='mp3')
